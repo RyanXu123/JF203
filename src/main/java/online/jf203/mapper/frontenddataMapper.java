ff23bedf95edcbe7d26d5597497ae23ba2dfdfd5
Kwokc233 <1765074429@qq.com>
2024-01-02 16:53:13 +0800

    Revert "新建实体类frontenddata"
    This reverts commit bb2c4cbf122e768bcc9201e67f9112f5481a587d.

--- src/main/java/online/jf203/mapper/frontenddataMapper.java
+++
@@ -1,9 +0,0 @@
-package online.jf203.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import online.jf203.entity.frontenddata;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface frontenddataMapper extends BaseMapper<frontenddata> {
-}
